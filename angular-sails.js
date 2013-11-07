dfd486707f23705458c17e2078be71a986a01791
Jan Pantel <jan.pantel@gmail.com>
2013-11-07 08:25:21 +0100

    Added reconnect, disconnect and emit functions

--- angular-sails.js
+++ angular-sails.js
@@ -7,18 +7,53 @@
  * file that was distributed with this source code.
  */
 
-(function (angular) {
+/*global angular, io */
+
+(function (angular, io) {
     'use strict';
     var ngSailsModule = angular.module('ngSails', []);
 
-    ngSailsModule.provider('$sails', $SailsProvider);
-
     function $SailsProvider() {
 
-        var socket = io.connect();
+        var socket = io.connect(),
+            connected = false,
+            reconnectAttempt = null;
+
+        socket.on('connect', function () {
+            connected = true;
+            if (typeof reconnectAttempt === 'function') {
+                reconnectAttempt();
+                reconnectAttempt = null;
+            }
+        });
+        socket.on('disconnect', function () {
+            connected = false;
+        });
 
         this.$get = function ($rootScope) {
             return {
+                reconnect: function (url, options) {
+                    var exec = function () {
+                        socket.disconnect();
+                        if (socket.socket !== undefined) {
+                            socket.socket.connect(url, options);
+                        }
+                    };
+                    //If we are connected, we can execute the reconnect right away.
+                    //If we are not, we queue it up, to execute it during to connect event occured.
+                    if (connected === true) {
+                        exec();
+                    } else {
+                        reconnectAttempt = exec;
+                    }
+
+                },
+                disconnect: function () {
+                    socket.disconnect();
+                },
+                emit: function (event, data) {
+                    socket.emit(event, data);
+                },
                 on: function (event, cb) {
                     socket.on(event, function () {
                         var args = arguments;
@@ -62,4 +97,7 @@
             };
         };
     }
-}(angular));
+
+    ngSailsModule.provider('$sails', $SailsProvider);
+
+}(angular, io));
